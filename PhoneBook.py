248a5247d3e22aafbc0b3d654eb1c9ac3b669e32
Fyodorov M <mf2004@inbox.ru>
2020-08-18 13:42:40 +0300

    добавление вывода в консоль

--- PhoneBook.py
+++ PhoneBook.py
@@ -8,18 +8,6 @@
 # {tel: [last_name, first_name, patronymic, address]}
 
 from PhoneBook_lib import *
-def input_data():
-    temp = list()
-    first_name = input("Введите имя: ")
-    last_name = input("Введите фамилию: ")
-    patronymic = input("Введите отчество: ")
-    address = input("Введите адрес: ")
-    temp.append(last_name)
-    temp.append(first_name)
-    temp.append(patronymic)
-    temp.append(address)
-    return temp
-
 
 phone_book = dict()
 
@@ -30,7 +18,7 @@ while True:
     choice = int(input("Введите режим работы: "))
 
     if choice == 1:
-        print(phone_book)
+        show(phone_book)
     elif choice == 2:
         tel = input("Введите номер телефона: ")
         if tel in phone_book:
@@ -39,20 +27,23 @@ while True:
         else:
             value = input_data()
             phone_book[tel] = value
-        value = input_data()
-        phone_book[tel] = value
     elif choice == 3:  # TODO Редактирование записи
         print()
     elif choice == 4:
         tel = input("Введите номер телефона для удаления: ")
         if tel in phone_book:
             note = phone_book.pop(tel)
-            print('Запись',note,'удалена')
+            print('Запись', note, 'удалена')
         else:
             print("Вы ввели неправильный номер")
             continue
-        print()
-    elif choice == 0:  # TODO Выход
+    elif choice == 5:
+        with open("PhoneBook.csv", "w") as file:
+            for tel in phone_book:
+                value = phone_book[tel]
+                temp = tel + ";" + value[0] + ";" + value[1] + ";" + value[2] + ";" + value[3]
+                file.write(temp)
+    elif choice == 0:
         print("До свидания")
         break
     else:
--- PhoneBook_lib.py
+++ PhoneBook_lib.py
@@ -9,14 +9,26 @@ def input_data():
     temp.append(patronymic)
     temp.append(address)
     return temp
+
+
 def welcome():
     print("*******")
     print("*** PhoneBook - телефонный справочник ***")
     print("*******")
+
+
 def menu():
     print("Режимы работы:")
     print("1. Показать все записи")
     print("2. Добавить запись")
     print("3. Редактировать запись")
     print("4. Удалить запись")
+    print("5. Сохранить в файл")
     print("0. Выход")
+
+
+def show(phone_book):
+    for tel in phone_book:
+        value = phone_book[tel]
+        temp = value[0] + " " + value[1] + " " + value[2] + ", " + value[3]
+        print(tel, ':', temp)
